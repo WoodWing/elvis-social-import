475983d2fb59d2c6aa7852eab882b1fff2bee2e7
Jaap van Blaaderen <jbl@woodwing.com>
2018-01-03 11:21:16 +0100

    Add more Twitter data to Elvis metadata + code cleanup

--- src/social/social-import.ts
+++ src/social/social-import.ts
@@ -10,7 +10,9 @@ export class SocialImport {
 
   private twitterClient: Twitter;
   private elvisApi: ElvisApi = ApiManager.getApi();
-
+  private topicConfig:any;
+  private topicsByKeyword:any = {};
+  
   constructor() {
     let args = {
       consumer_key: Config.twitterConsumerKey,
@@ -20,15 +22,34 @@ export class SocialImport {
     }
 
     this.twitterClient = new Twitter(args);
-    let keywords:string[] = ['KatiePrice', 'MeghanMarkle', 'sneeuw', 'trump'];
-    this.searchTwitter(keywords);
+
+    this.topicConfig =  {
+      'Grammys' : ['Grammys', 'Grammys2018'],
+      'Golden Globes' : ['GoldenGlobes'],
+      'Sag Awards' : ['SAGAwards'],
+      'Rohingya' : ['Rohingya', 'RHONJ'],
+      'California Wildfire' : ['CaliforniaWildfire', 'CaliforniaFire'],
+      'Star Wars The Last Jedi' : ['StarWarsTheLastJedi'],
+      'Roy Moore' : ['RoyMoore'],
+      'Ivana Smit' : ['IvanaSmit'],
+      'Salma Hayek' : ['SalmaHayek']
+    }
+
+    // Create reversed map for quick lookup
+    for (let [topic, keywords] of Object.entries(this.topicConfig)) {
+      for (let keyword of keywords) {
+        this.topicsByKeyword[keyword] = topic;
+      }
+    }
+
+    this.searchTwitter();
   }
 
-  private searchTwitter(keywords:string[]):void {
-    let searchFor:string = keywords.join(',')
+  private searchTwitter():void {
+    let searchFor:string = Object.keys(this.topicsByKeyword).join(',');
     this.twitterClient.stream('statuses/filter', {track: searchFor},  (stream) => {
       stream.on('data', (tweet) => {
-        this.downloadTweetMedia(tweet, keywords);
+        this.downloadTweetMedia(tweet);
       });
       stream.on('error', (error) => {
         console.log(error);
@@ -36,10 +57,10 @@ export class SocialImport {
     });
   }
 
-  private async downloadTweetMedia(tweet:any, keywords:string[]):Promise<void> {
+  private async downloadTweetMedia(tweet:any):Promise<void> {
     if (tweet.entities && tweet.entities.media && !tweet.retweeted_status) {
       let media = tweet.entities.media;
-      let foundKeyword:string = this.findKeyword(tweet.text, keywords);
+      let foundKeyword:string = this.findKeyword(tweet.text);
       media.forEach(async mediaItem => {
         console.log('Found a ' + mediaItem.type + ' searching for "' + foundKeyword + '". Tweet url: ' + mediaItem.url + ' Tweet ' + mediaItem.type + ': ' +  mediaItem.media_url_https);
         let buffer:Buffer = await this.downloadFile(mediaItem.media_url_https);
@@ -49,24 +70,22 @@ export class SocialImport {
           let hit:HitElement = await this.elvisApi.create(buffer, JSON.stringify(metadata));
           console.log('Downloaded and imported: ' + hit.metadata.assetPath);
         }
-        else {
-          console.log('Skipped import, file already in Elvis: ' + mediaItem.media_url_https);
-        }
       });
     }
   }
 
-  private findKeyword(tweetTxt:string, keywords:string[]):string {
+  private findKeyword(tweetTxt:string):string {
     let tweetWords:string[] = tweetTxt.toLowerCase().match(/[^\s]+/g);
-    for(let keyword of keywords) {
+    let keywordMap = Object.entries(this.topicsByKeyword);
+    for (let [keyword, topic] of keywordMap) {
       if(tweetWords.includes(keyword.toLowerCase()) || tweetWords.includes('#' + keyword.toLowerCase())) {
-        return keyword;
+        return topic;
       }
     }
     // Not found on exact word match, fallback to partial matching
-    for(let keyword of keywords) {
+    for (let [keyword, topic] of keywordMap) {
       if(tweetTxt.toLowerCase().indexOf(keyword) > -1) {
-        return keyword;
+        return topic;
       }
     }
     return '';
@@ -109,7 +128,11 @@ export class SocialImport {
       created: tweet.created_at,
       termsAndConditionsURL: 'https://twitter.com/en/tos',
       description: tweet.text,
-      tags: []
+      tags: [],
+      imageCreator: tweet.user.name,
+      imageCreatorID: tweet.user.id_str,
+      imageCreatorImageID: mediaItem.id_str,
+      creatorName: tweet.user.name
     };
 
     if (tweet.entities.hashtags) {
@@ -119,11 +142,38 @@ export class SocialImport {
       metadata['tags'].push(foundKeyword);
     }
 
-    if (tweet.coordinates && tweet.coordinates.length == 2) {
-      metadata['longitude'] = tweet.coordinates[0];
-      metadata['latitude'] = tweet.coordinates[1];
+    let hasExactCoordinates:boolean = (tweet.coordinates && tweet.coordinates.length == 2);
+
+    if (tweet.place) {
+      let place:any = tweet.place;
+      if (place.place_type === 'city') {
+        metadata['shownCity'] = place.name;
+      }
+      metadata['shownCountryName'] = place.country;
+      metadata['shownCountryCode'] = place.country_code;
+
+      let bb:any = place.bounding_box;
+       // Use boundingbox coordinates if we don't have exact coordinates and if they exist
+      if (!hasExactCoordinates && bb.coordinates && bb.coordinates.length == 1 && bb.coordinates[0].length == 4 && bb.type === 'Polygon') {
+        let c:number[] = bb.coordinates[0];
+        metadata['gpsLongitude'] = (c[0][0] + c[2][0]) / 2;
+        metadata['gpsLatitude'] = (c[0][1] + c[1][1]) / 2;
+        console.log('BOUNDING BOX: longitude: ' + metadata['gpsLongitude'] + ' latitude: ' + metadata['gpsLatitude'] + ' sourceId:"' + tweet.id_str + '"');
+      }
     }
-        
+    if (hasExactCoordinates) {
+      metadata['gpsLongitude'] = tweet.coordinates[0];
+      metadata['gpsLatitude'] = tweet.coordinates[1];
+      console.log('EXACT LOCATION: longitude: ' + metadata['gpsLongitude'] + ' latitude: ' + metadata['gpsLatitude'] + ' sourceId:"' + tweet.id_str + '"');
+    }
+    
+    if (tweet.user.location) {
+      metadata['creatorAddress'] = tweet.user.location;
+    }
+    if (tweet.user.url) {
+      metadata['website'] = tweet.user.url;
+    }
+
     return metadata;
   }
 }
